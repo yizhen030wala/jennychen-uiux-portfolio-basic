00628442993275f80ba8f5fe40221593950c0a1f
Jenny Chen <146735072+yizhen030wala@users.noreply.github.com>
2024-01-09 14:23:08 +0800

    Add files via upload
    add code 5

---
+++ framerusercontent.com/sites/myJS.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loadingAnimation = document.getElementById("loadingAnimation");
+    const mainContent = document.getElementById("main");
+  
+    loadingAnimation.classList.add("show"); // 加上顯示動畫的 CSS class
+  
+    // 延遲兩秒後隱藏動畫
+    setTimeout(function() {
+      loadingAnimation.classList.remove("show"); // 移除顯示動畫的 CSS class
+      mainContent.style.display = "block"; // 顯示主要內容
+    }, 2000);
+  });
+  
