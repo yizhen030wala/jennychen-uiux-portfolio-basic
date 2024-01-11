9c3bf91e8c97f11bd3399a2c5e798a8eec127f16
yizhen030wala <yizhenchen06@gmail.com>
2024-01-11 14:47:06 +0800

    change scroll bar

--- framerusercontent.com/sites/1wJ1JbFYFYuL3IdBFEXsMj/chunk-GBRBWYWU.mjs
+++
@@ -1,2 +0,0 @@
-import{m as a,o}from"./chunk-IDW6ITFS.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
-//# sourceMappingURL=chunk-GBRBWYWU.mjs.map
---
+++ framerusercontent.com/sites/1wJ1JbFYFYuL3IdBFEXsMj/chunk-I4UTKQUZ.mjs
@@ -0,0 +1,2 @@
+import{m as a,o}from"./chunk-AKEY5Z52.mjs";var r="default"in o?a:o,t={},e=r;t.createRoot=e.createRoot;t.hydrateRoot=e.hydrateRoot;var s=t.createRoot,f=t.hydrateRoot;export{s as a,f as b};
+//# sourceMappingURL=chunk-I4UTKQUZ.mjs.map
