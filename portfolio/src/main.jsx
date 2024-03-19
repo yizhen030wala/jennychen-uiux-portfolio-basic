22ddbf9a88ca98de6c35f1c6fc64c91bbbdcd8ad
Jenny Chen <yizhenchen06@gmail.com>
2024-03-19 22:48:57 +0800

    0319 update

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+
+import Home from '../src/pages/home/index'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
--- portfolio/src/pages/home/index.jsx
+++ portfolio/src/pages/home/index.jsx
@@ -1,11 +1,55 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Header from '../../../public/nav/nav'
+
+//img
+import TextDesk from '../../assets/images/textDesk.png'
 
 const index = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+      <main>
+        <div className="hero">
+          <div className="text_wrap">
+            <h1>
+              Hi ! I’m Jenny!
+              A Graphic/Visual Designer
+            </h1>
+            <figure><img src={TextDesk} alt="" /></figure>
+          </div>
+          <div className="btn_wrap">
+            <button className="btn_black">關於我</button>
+            <button className="btn_white">下載履歷 <img src={} alt="" /></button>
+          </div>
+        </div>
+
+        <div className="works">
+          <div className="title"><img src={} alt="" /></div>
+
+          <div className="container">
+            {/* one */}
+            <div className="card">
+              <div className="left"><img src={} alt="" /></div>
+              <div className="right">
+                <div className="info_top">
+                  <h4>2022</h4>
+                  <h1>Reto旅藤</h1>
+                  <h4>旅藤是一款如何如何的作品</h4>
+                  <div className="tag_wrap">
+                    <div className="tag">UIUX</div>
+                    <div className="tag">原型設計</div>
+                    <div className="tag">設計規範</div>
+                  </div>
+                </div>
+                <button className="info_btn">了解更多細節</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+    </>
   )
 }
 
